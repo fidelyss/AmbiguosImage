b3271334be66132ca2fca04913d4555f37729cef
Alisson <alissonfidelis147@gmail.com>
2025-02-14 17:04:32 -0300

    config webpack concluida

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import './styles/index.css'
+export default function App() {
+    return (<>
+        <div className="container">dasdas</div>
+        <div className="container">dasdas</div>
+    </>)
+}
---
+++ webpack.config.cjs
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ],
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 5500, // Ou qualquer outra porta
+        historyApiFallback: true, // Importante para React Router
+    }
+
+};
